0a7bda02dc0c94672af736f2598adb2586150a1d
chenhy <chenhy@caibab.com>
2019-01-28 17:29:41 +0800

    测试

---
+++ cloud-common/src/main/java/com/ocean/cloudcommon/CloudCommonApplication.java
@@ -0,0 +1,14 @@
+package com.ocean.cloudcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCommonApplication.class, args);
+	}
+
+}
+
